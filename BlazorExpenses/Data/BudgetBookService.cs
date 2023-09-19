f4e0fead0cb1c87bfed27f789813c0842a849c16
cookieeszn <klauscity01@gmail.com>
2023-09-19 13:31:08 -0700

    Add project files.

---
+++ BlazorExpenses/Data/BudgetBookService.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorExpenses.Data
+{
+    public class BudgetBookService
+	{
+        private static List<Entry> dummyDB = new List<Entry>();
+
+        public async Task<bool> AddEntry(Entry newEntry)
+        {
+            dummyDB.Add(newEntry);
+            return true;
+        }
+
+        public async Task<List<Entry>> GetAllEntries()
+        {
+            return dummyDB;
+        }
+    }
+}
